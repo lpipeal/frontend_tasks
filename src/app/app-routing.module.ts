5ec3d6a720df6e642b32a82552cdcc853e67d090
luis felipe alvarez <lpipeal@gmail.com>
2020-06-26 03:23:07 -0500

    Cambio en estructura de proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TaskComponent } from './components/task/task.component';
+import { TaskComponent } from './components/task/components/task.component';
 
 
 const routes: Routes = [
