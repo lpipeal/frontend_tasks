5941461f1317db7f97e6af2ecb61abff07370a3b
luis felipe alvarez <lpipeal@gmail.com>
2020-06-26 03:25:06 -0500

    Agregando httpclientmodule y el taskservice al providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/page/header/header.component';
 import { FooterComponent } from './components/page/footer/footer.component';
-import { TaskComponent } from './components/task/task.component';
+import { TaskComponent } from './components/task/components/task.component';
+import { TaskService } from './components/task/service/task.service';
+
+import { HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    TaskComponent
+    TaskComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
